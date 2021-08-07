1bfa2d44851b85b6744a1306f89a41a9bc23a3cd
Hector Franco T <tor6.franco6@hotmail.com>
2021-08-06 19:41:06 -0500

    Add 07: Read and validate

---
+++ 02-interactive-scripts/07-read-and-validation.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+option=0
+file=""
+
+echo -e "--- Welcome ---\n"
+
+read -p "Choose an option: " option
+read -p "What is the name file? " file
+
+echo "Option: $option. File: $file"
+
